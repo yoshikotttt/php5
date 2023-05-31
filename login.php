a25fbb647b65502a1eb6ffd5cdebf48b188feb04
yoshikotttt <myqic.n@gmail.com>
2023-05-31 21:45:35 +0900

    修正

--- login.php
+++ login.php
@@ -25,43 +25,8 @@
         </div>
     </form>
 
-    <script>
+ 
 
-
-const friendAddress = "友達の住所";
-const success = true; // 手紙が投函できたかどうか（仮の値）
-
-
-const getAddress = () => {
-  return new Promise((resolve, reject) => {
-     if (friendAddress) {
-      resolve(friendAddress);
-    } else {
-      reject("住所が見つかりません");
-    }
-  });
-};
-
-const sendLetter = (address) => {
-  return new Promise((resolve, reject) => {
-    // 手紙をポストに投函する処理
-    if (success) {
-      resolve("手紙が送信されました");
-    } else {
-      reject("手紙の送信に失敗しました");
-    }
-  });
-};
-
-getAddress()
-  .then((address) => sendLetter(address))
-  .then((result) => {
-    console.log(result); // "手紙が送信されました"
-  })
-  .catch((error) => {
-    console.error(error); // エラーメッセージが表示されます
-  });
-    </script>
 </body>
 
 </html>
